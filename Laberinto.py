339cb7d836e66681ee828660883f93d489657deb
Tatiana Barbosa <tstianabarbosa@gmail.com>
2023-10-18 18:28:19 -0500

    git pull

---
+++ Laberinto.py
@@ -0,0 +1,16 @@
+import  readchar
+from readchar import key
+nombre_jugador = input("Introduce tu nombre: ")
+mensaje = "Bienvenid@ al juego"
+print("¡Hola" + nombre_jugador + "!")
+print(mensaje)
+
+while True:
+    tecla_2 = readchar.readkey()
+    if tecla_2 == key.UP:
+        break
+    print(tecla_2)
+
+
+
+

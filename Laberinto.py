555f96b36b56ed789f4085d05153d3ba27dc34a8
Tatiana Barbosa <tstianabarbosa@gmail.com>
2023-10-23 14:21:24 -0500

    git pull

--- Laberinto.py
+++ Laberinto.py
@@ -1,23 +1,32 @@
-import  readchar
+import readchar
+import os
 from readchar import key
-nombre_jugador = input("Introduce tu nombre: ")
-mensaje = "Bienvenid@ al juego"
-print("¡Hola" + nombre_jugador + "!")
-print(mensaje)
 
-while True:
-    tecla_2 = readchar.readkey()
-    if tecla_2 == key.UP:
-        break
-    print(tecla_2)
+def clear_terminal():
+    os.system('cls' if os.name=='nt' else 'clear')
 
+def main():
+    nombre_jugador = input("Introduce tu nombre: ")
+    mensaje = "Bienvenid@ al juego"
+    print("¡Hola" + nombre_jugador + "!")
+    print(mensaje)
+    
+    print("para salir del bucle, presiona la flecha arriba")
 
+    while True:
+        tecla_2 = readchar.readkey()
+        if tecla_2 == key.UP:
+            break
+        print(tecla_2)
 
+    clear_terminal()
+    numero = 0 
+    while numero < 50:
+        input("Presiona la tecla 'n' para continuar...")
+        clear_terminal()
+        numero += 1
+        print(numero)
 
-
-nombre_jugador = input("Introduce tu nombre: ")
-mensaje = "Bienvenid@ al juego"
-print("¡Hola" + nombre_jugador + "!")
-print(mensaje)
-
+if __name__ == "__main__":
+    main()
 
